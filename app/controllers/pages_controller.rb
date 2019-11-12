da3dace9d22503e357e4791772072fc500e92eec
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-11-12 15:22:35 +1100

    updated controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :about]
 
   def home
+  end
+
+  def about
+  end
+
+  def wheel
+
+    # @project = Project.find(params[:id])
+
+    @hardelements = Hardelement.all
+    @softelements = Softelement.all
+
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,27 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to project_path(@project)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :address)
+  end
 end
